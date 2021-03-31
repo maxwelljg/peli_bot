f32cc00aff6ff5c77a5e9683688faa9634f9a1e2
Maxwell Gold <mgold@luc.id>
2021-03-31 16:59:49 -0400

    Fixing headers

--- pelibot.py
+++ pelibot.py
@@ -28,7 +28,19 @@ class MyClient(discord.Client):
             return
           
         if message.content.startswith('pb.roster'):
-            headers = {'Referer': 'https://stats.nba.com/', 'x-nba-stats-origin': 'stats', 'x-nba-stats-token': 'true'}
+            headers = {
+                'Host': 'stats.nba.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'x-nba-stats-origin': 'stats',
+                'x-nba-stats-token': 'true',
+                'Connection': 'keep-alive',
+                'Referer': 'https://stats.nba.com/',
+                'Pragma': 'no-cache',
+                'Cache-Control': 'no-cache',
+    }
             url = 'https://stats.nba.com'
             session = aiohttp.ClientSession()
             statResponse = await session.get(url, headers=headers)
