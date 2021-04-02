6172a444abf1a030a5c4575350b6ae4682b52413
young-lu <cyl28@georgetown.edu>
2021-04-02 18:53:37 -0400

    next schedule cmd (untested)

--- pelibot.py
+++ pelibot.py
@@ -1,6 +1,12 @@
 # pelibot.py
 import discord
+import json
 import asyncio
+import aiohttp
+import requests
+from prettytable import PrettyTable
+from fuzzywuzzy import process
+from disctoken import botToken
 
 class MyClient(discord.Client):
   
@@ -9,6 +15,8 @@ class MyClient(discord.Client):
 
         # create the background task and run it in the background
         self.bg_task = self.loop.create_task(self.my_background_task())
+		self.initDict()
+		self.teamId = '1610612740'
         
     async def on_ready(self):
         print('Logged in as')
@@ -22,8 +30,24 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith('!hello'):
-            await message.reply('Hello!', mention_author=True)
+        if message.content.startswith('!next'):
+            # await message.reply('Hello!', mention_author=True)
+            # make request for schedule and reply with the next game details.
+            year = 2020
+			url = "http://data.nba.net/prod/v1/{}/teams/{}/schedule.jsob".format(year, self.teamId)
+			session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
+			statResponse = await session.get(url)
+			statBlob = await statResponse.json()
+            await session.close()
+			data = ['league']['standard'][int(data['league']['lastStandardGamePlayedIndex'])+1]
+            st = datetime.datetime(data['startTimeEastern'])
+            gamecode = data['gameUrlCode'].split('/')
+            date = datetime.datetime.strptime(gamecode[0], '20%y%m%d')
+            opp_tricode = gamecode[1][:3] if gamecode[1][-3:] == 'NOP' else gamecode[1][-3:]
+            t = PrettyTable(['OPP', 'DATE', 'TIME'])
+            t.add_row([opp_tricode, '{} {}/{}'.format(date.strftime('%a'), data[1],date[2]), st])
+            embedVar = discord.Embed(title="Next Scheduled Game", description=str(t), color=0x00ff00)
+            await message.channel.send(embed=embedVar)
             return
   
     # example background task
