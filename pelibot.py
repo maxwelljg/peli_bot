9e65526eaf40530454ff509a0193f07b003af3f9
young-lu <cyl28@georgetown.edu>
2021-04-05 15:28:37 -0400

    next schedule command

--- pelibot.py
+++ pelibot.py
@@ -7,6 +7,7 @@ import requests
 from prettytable import PrettyTable
 from fuzzywuzzy import process
 from disctoken import botToken
+from datetime import datetime
 
 class MyClient(discord.Client):
   
@@ -15,7 +16,10 @@ class MyClient(discord.Client):
 
         # create the background task and run it in the background
         self.bg_task = self.loop.create_task(self.my_background_task())
-		self.initDict()
+        self.skcID = 503665006961754113
+        self.gamedayID = 787145727846776872
+        self.botspamID = 503665658374914069
+        self.serverID = 503662263337484310
 		self.teamId = '1610612740'
         
     async def on_ready(self):
@@ -34,18 +38,19 @@ class MyClient(discord.Client):
             # await message.reply('Hello!', mention_author=True)
             # make request for schedule and reply with the next game details.
             year = 2020
-			url = "http://data.nba.net/prod/v1/{}/teams/{}/schedule.jsob".format(year, self.teamId)
+			url = "http://data.nba.net/prod/v1/{}/teams/{}/schedule.json".format(year, self.teamId)
 			session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
 			statResponse = await session.get(url)
 			statBlob = await statResponse.json()
             await session.close()
-			data = ['league']['standard'][int(data['league']['lastStandardGamePlayedIndex'])+1]
-            st = datetime.datetime(data['startTimeEastern'])
+			data = statBlob['league']['standard'][int(data['league']['lastStandardGamePlayedIndex'])+1]
+            # starttime as easter time string
+            st = data['startTimeEastern']
             gamecode = data['gameUrlCode'].split('/')
-            date = datetime.datetime.strptime(gamecode[0], '20%y%m%d')
+            date = datetime.strptime(gamecode[0], '20%y%m%d')
             opp_tricode = gamecode[1][:3] if gamecode[1][-3:] == 'NOP' else gamecode[1][-3:]
             t = PrettyTable(['OPP', 'DATE', 'TIME'])
-            t.add_row([opp_tricode, '{} {}/{}'.format(date.strftime('%a'), data[1],date[2]), st])
+            t.add_row([opp_tricode, '{} {}/{}'.format(date.strftime('%a'), date.month, date.day), st])
             embedVar = discord.Embed(title="Next Scheduled Game", description=str(t), color=0x00ff00)
             await message.channel.send(embed=embedVar)
             return
@@ -59,5 +64,7 @@ class MyClient(discord.Client):
             await channel.send('testing background task')
             await asyncio.sleep(3600) # task runs every hour
 
-client = MyClient()
-client.run('token')
+intents = discord.Intents.default()
+intents.members = True
+client = MyClient(intents = intents)
+client.run(botToken)
